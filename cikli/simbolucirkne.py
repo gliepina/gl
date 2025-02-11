decb260558b0836d4280baf8fac7dcfc78da0076
gliepina <gliepina07@gmail.com>
2025-02-11 07:41:57 +0000

    v04

---
+++ cikli/simbolucirkne.py
@@ -0,0 +1,10 @@
+virkne1 = input()
+virkne1.split(" ") 
+print(virkne1.split)
+virkne1.count("a")
+print(virkne1.count)
+*4 uzd*
+vardi=teikums.split(" ")
+for i in range(len(vardi)-1):
+    print(len(vardi[i]))
+print(len(vardi[len(vardi)-1]-1))
